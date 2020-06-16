56cd20cd88be5b2def6ccbf45e1292d601b7819e
len23 <lenin.montalvo.23@hotmail.com>
2020-06-16 16:51:34 -0500

    Advancing in functionality

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
+const RESTRCTIONS = {
+  timeRestriction:{
+    morning: '7:00 - 9:30',
+    evening: '16:00 - 19:30'
+  },
+  monday:{
+    plate: [1,1]
+  },
+  tuesday:{
+    plate: [3,4]
+  },
+  wednesday: {
+    plate: [5,6]
+  },
+  thursday: {
+    plate: [7,8]
+  },
+  friday: {
+    plate: [9,0]
+  }
+}
+
 const picoPlaca = (plate,date,time)=>{
 
-  let datePico = new Date(`${date},${time}`);
- 
-  
+  let lastDigit = plate.charAt(plate.length-1);
+  let day = new Date(date).getDay()
+
+  console.log(lastDigit, day);
   
 
 }
