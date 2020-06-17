bf11981e1b541f9f5a8141fd11bda58af7333ad2
len23 <lenin.montalvo.23@hotmail.com>
2020-06-17 00:11:59 -0500

    Restrictions

--- resources/js/controllers.js
+++ resources/js/controllers.js
@@ -1,32 +1,3 @@
-const RESTRCTIONS = {
-  timeRestriction:{
-    morning: '7:00 - 9:30',
-    evening: '16:00 - 19:30'
-  },
-  monday:{
-    plateDigit: [1,2]
-  },
-  tuesday:{
-    plateDigit: [3,4]
-  },
-  wednesday: {
-    plateDigit: [5,6]
-  },
-  thursday: {
-    plateDigit: [7,8]
-  },
-  friday: {
-    plateDigit: [9,0]
-  },
-  saturday: {
-    plateDigit: []
-  },
-  sunday: {
-    plateDigit: []
-  }
-}
-
-const DAYS = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
 
 
 //Class for plate
