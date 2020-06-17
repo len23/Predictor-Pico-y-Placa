e69f5fad7a9837a31c1e8c9799719f99b654a0a1
len23 <lenin.montalvo.23@hotmail.com>
2020-06-16 20:03:21 -0500

    User Interface Ready

--- controllers.js
+++ controllers.js
@@ -5,7 +5,7 @@ class Plate  {
   this.time = hour.toString();
  } 
 
-  predictPicoPlaca (){
+  predictPicoPlaca (callback) {
   let dateDay = new Date(`${this.date},${this.time}`);
   let lastDigit = parseInt(this.plate.charAt(this.plate.length-1));
   let day = DAYS[dateDay.getDay()];
@@ -17,23 +17,29 @@ class Plate  {
     
   if(rest.includes(lastDigit)){
     if((hourDay>=7 && hourDay<=9) || (hourDay>=16 && hourDay<=19)){
-        if((hourDay==9 || hourDay===19) && (minHour<=30)){
-          return `You can't be on the road`;
-        }else{
-          return `You can be on the road`;
+        if((hourDay===9 || hourDay===19) && (minHour>=30)){
+          return callback (true, `You can be on the road`);
         }
+          return callback (false, `You can't be on the road`);
+        
     }else{
-      return `You can be on the road`;
+      return callback (true, `You can be on the road`);
     }
     
   }
-  return 'You can be on the road';
+  return callback (true, `You can be on the road`);
+  console.log('Llega hasta aca');
+  
 }
 }
 
 class UI { 
-  showPredict(param){
-    document.getElementById('predict').innerText=param;
+  showAlert(message, className){
+    document.getElementById(className).innerText=message;
+      // Timeout after 3 sec
+      setTimeout(function(){
+        document.querySelector('#error').innerText='';
+      }, 3000);
   }
   clearFields() {
     document.getElementById('plate').value = '';
@@ -47,10 +53,32 @@ document.getElementById('plate-form').addEventListener('submit', function(e){
   const plate = document.getElementById('plate').value,
   date = document.getElementById('date').value,
   time = document.getElementById('time').value
+  // Instantiate UI
+  const ui = new UI();
 
-   // Instantiate plate
-   const plateObj = new Plate(plate, date, time);
-   document.getElementById('predict').innerText = plateObj.predictPicoPlaca();
+  if(plate === '' || date === '' || time === ''){
+   // Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  }else {
+       // Instantiate plate
+        const plateObj = new Plate(plate, date, time);
+        //Get the message section
+        const msg =  document.getElementById('predict');
+         plateObj.predictPicoPlaca((e,i)=>{
+            if(e){
+              msg.style.color = '#0cb870';
+              msg.innerText=i
+            }else{
+              msg.style.color = '#c72b2ba4';
+              msg.innerText=i
+            }
+
+        });
+        setTimeout(function(){
+          document.querySelector('#predict').innerText='';
+        }, 2000);
+  }
+   
 
 });
 
